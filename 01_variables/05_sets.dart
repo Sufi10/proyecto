b347c46394d1f2a4122603c8b911cd085811cb86
Sufi10 <114923826+Sufi10@users.noreply.github.com>
2022-10-06 10:55:12 +0200

    05_sets

--- 01_variables/05_sets.dart
+++ 01_variables/05_sets.dart
@@ -1,1 +1,9 @@
-void main() {}
+void main() {
+  List<String> palabras = ["hola", "adios", "buenas"];
+  print(palabras);
+  Set<String> palabras2 = palabras.toSet();
+  palabras2.add("olaaaaaa");
+  print(palabras2);
+  List<String> palabras3 = palabras2.toList();
+  print(palabras3);
+}
